c556777572089c47e874b07ca5792114ebae52b9
cielivree <cielivree93@gmail.com>
2025-09-28 17:53:37 +0300

    Use typed selector

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useTypedSelector } from '../hooks/useTypedSelector'
 
 const UserList: React.FC = () => {
-  const state = useSelector(state => state)
-  console.log(state)
+  const {users, isLoading, error} = useTypedSelector(state => state.user)
+  console.log(users)
 
   return (
     <div>UserList</div>
